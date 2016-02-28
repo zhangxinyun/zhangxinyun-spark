b921bc553c754b95ed04594f4c7117db9efc6897
Anthony Li <lizhen.dgut@gmail.com>
2016-02-28 17:56:42 +0800

    IDA 初次提交

---
+++ lda.py
@@ -0,0 +1,34 @@
+#encoding:utf-8
+from pyspark.mllib.clustering import LDA, LDAModel
+from pyspark.mllib.linalg import Vectors
+import jieba
+
+import settings
+
+def spark_context(master):
+    conf = SparkConf().setAppName('zhangxinyun-spark').setMaster(master)
+    sc = SparkContext(conf=conf)
+    return sc
+
+def main():
+    # 初始化 SparkContext
+    sc = spark_context(spark_master)
+    
+    # Load and parse the data
+    data = sc.textFile(hdfs_path)
+    parsedData = data.map(lambda line: Vectors.dense([float(x) for x in line.strip().split(' ')]))
+    # Index documents with unique IDs
+    corpus = parsedData.zipWithIndex().map(lambda x: [x[1], x[0]]).cache()
+
+    # Cluster the documents into three topics using LDA
+    ldaModel = LDA.train(corpus, k=3)
+
+    # Output topics. Each is a distribution over words (matching word count vectors)
+    print("Learned topics (as distributions over vocab of " + str(ldaModel.vocabSize()) + " words):")
+    topics = ldaModel.topicsMatrix()
+    for topic in range(3):
+        print("Topic " + str(topic) + ":")
+        for word in range(0, ldaModel.vocabSize()):
+            print(" " + str(topics[word][topic]))
+
+main()
