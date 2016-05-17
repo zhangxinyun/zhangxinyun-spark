434026a38208e3cd9dd7205e7c133315a61d22f7
dyingbleed <lizhen.dgut@gmail.com>
2016-05-17 23:56:33 +0800

    add: store data into mongodb

--- tfidf.py
+++ tfidf.py
@@ -2,7 +2,7 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.feature import HashingTF, IDF
 
-from settings import spark_master, hdfs_path
+from settings import spark_master, hdfs_path, mongo_host, mongo_user, mongo_pass
 
 def spark_context(master):
     conf = SparkConf().setAppName('zhangxinyun-spark').setMaster(master)
@@ -10,9 +10,27 @@ def spark_context(master):
     return sc
 
 def tokenize(line):
-    import jieba.posseg
+    from jieba import posseg
     terms = posseg.cut(line)
     return [term for term, flag in terms if flag in ['an', 'n', 'nz', 'vn']];
+
+def doc_tfidf(item):
+    result = []
+    terms = item[0]
+    tfidf_array = item[1].toArray()
+    numFeatures = 1 << 20
+    for term in set(terms):
+        index = hash(term) % numFeatures
+        tfidf = tfidf_array[index]
+        if tfidf > 0.0:
+            result.append((term, tfidf))
+    return sorted(result, key=lambda i: i[1])
+
+def clear_mongodb(client):
+    client.zxy.terms.delete_many({})
+
+def send_mongodb(client, doc):
+    client.zxy.terms.insert_one(doc)
 
 def main():
     # 初始化 SparkContext
@@ -23,16 +41,34 @@ def main():
 
     # 分词
     documents = data.map(tokenize)
+    documents.cache()
 
     # TF
     hashingTF = HashingTF()
     tf = hashingTF.transform(documents)
-    tf.cache()
 
     # IDF
     idf = IDF(minDocFreq=2).fit(tf)
     
     # TFIDF
-    #tfidf = idf.transform(tf)
+    tfidf = idf.transform(tf)
+
+    # 链接到 MongoDB
+    from pymongo import MongoClient
+    mongo_client = MongoClient(mongo_host)
+    mongo_client.admin.authenticate(mongo_user, mongo_pass, mechanism='SCRAM-SHA-1')
+    clear_mongodb(mongo_client)
+
+    # zip
+    term_tfidf = documents.zip(tfidf).map(doc_tfidf)
+    for article in term_tfidf.collect():
+        data = {}
+        data['terms'] = []
+        for term in article:
+            item = {}
+            item['text'] = term[0].encode('utf-8')
+            item['size'] = int(term[1] * 100)
+            data['terms'].append(item)
+        send_mongodb(mongo_client, data)
 
 main()
